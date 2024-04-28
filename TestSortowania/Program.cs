0008e45a4d2e134642ce79a9449905bd6cd8eec8
kamil <kamilkazmierczak10@gmail.com>
2024-04-28 17:08:48 +0200

    Dodaj pliki projektów.

---
+++ TestSortowania/Program.cs
@@ -0,0 +1,28 @@
+﻿    using Sortowanie;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] liczby = new int[10];
+        Random r = new Random();
+        Console.WriteLine($"Tablica przed sortowaniem:");
+        for (int i = 0; i < liczby.Length; i++)
+        {
+            liczby[i] = r.Next(0, 1001);
+            Console.Write(liczby[i]);
+        }
+        QuickSort.sortuj(liczby);
+
+        Console.WriteLine("Tablica po sortowaniu");
+        foreach (int i in liczby)
+        {
+            Console.WriteLine(i);
+        }
+        Console.ReadKey();
+
+
+
+    }
+}
